c99726663403dab4d4311ff5fee81beb17150ac6
Yosafat Kesuma <da005kage@gmail.com>
2020-04-11 14:42:58 +0700

    home

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -49,13 +49,13 @@
         <div class="row align-items-center">
           
           <div class="col-11 col-xl-2">
-            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black h2 mb-0">Create<span class="text-primary">.</span> </a></h1>
+            <h1 class="mb-0 site-logo"><a href="{{ route('index') }}" class="text-black h2 mb-0">Create<span class="text-primary">.</span> </a></h1>
           </div>
           <div class="col-12 col-md-10 d-none d-xl-block">
             <nav class="site-navigation position-relative text-right" role="navigation">
 
               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li><a href="#home-section" class="nav-link">Home</a></li>
+                <li><a href="{{ route('index') }}" class="nav-link">Home</a></li>
                 <li><a href="#services-section" class="nav-link">Services</a></li>
                 <li><a href="#about-section" class="nav-link">About</a></li>
                 <li><a href="#blog-section" class="nav-link">Blog</a></li>
